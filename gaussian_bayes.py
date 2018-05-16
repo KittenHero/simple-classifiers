bb695a9b8a8500cbe0c414dc72c2beb75d496b17
Naris <nariscatboy@gmail.com>
2018-05-16 14:11:52 +1000

    representation for NB

--- gaussian_bayes.py
+++ gaussian_bayes.py
@@ -9,11 +9,16 @@ from functools import reduce
 from collections import OrderedDict, defaultdict
 from itertools import groupby
 from operator import itemgetter, mul
+from pprint import pformat, pprint
 
 class GaussianModel:
-    def __init__(self, data):
-        self.mean = stat.mean(data)
-        self.variance = stat.variance(data)
+    def __init__(self, mean, variance):
+        self.mean = mean
+        self.variance = variance
+
+    @classmethod
+    def from_data(cls, data):
+        return cls(stat.mean(data), stat.variance(data))
 
     def probability(self, x):
         return self.gaussian(x, self.mean, self.variance)
@@ -22,6 +27,9 @@ class GaussianModel:
     def gaussian(x, m, v):
         'computes the gaussian at x parameterised by the mean and variance'
         return math.exp( -(x - m)**2 / (2*v) ) / math.sqrt( 2*math.pi*v )
+
+    def __repr__(self):
+        return '{cls}(mean={m}, variance={v})'.format(cls=self.__class__.__name__, m=self.mean, v=self.variance)
 
 
 class GaussianNaiveBayes:
@@ -52,7 +60,7 @@ class GaussianNaiveBayes:
                 # extract the ith attribute
                 attr = list(map(itemgetter(i), data))
                 # store the model on the class
-                self.model[label][i] = GaussianModel(attr)
+                self.model[label][i] = GaussianModel.from_data(attr)
 
     def classify(self, p):
         'classify the point with the from known data'
@@ -69,3 +77,15 @@ class GaussianNaiveBayes:
 
     def classify_many(self, *points):
         return map(self.classify, points)
+
+    def __repr__(self):
+        return '{}(\nclasses={}\n)'.format(self.__class__.__name__, pformat(dict(self.model.items())))
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from csv import reader
+
+    arg = ArgumentParser()
+    arg.add_argument('datafile', type=open, help='CSV file containing numeric attributes and a class to train the model')
+    pprint(GaussianNaiveBayes(reader(arg.parse_args().datafile)))
